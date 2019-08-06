1b38e9a336f58318a0a721d602803875113a98b0
OHFloree <2001.florian.mueller@gmail.com>
2019-08-06 16:17:12 +0200

    solution backend

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const routes = require('./routes/routes.js');
+var session = require('express-session');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(session({secret: 'max', saveUnitialized: false, resave: false}));
 
-routes(app);
+const colorService = require('./services/colorService')
+const colors = new colorService;
+const solution = colors.getSolution(8);
+
+app.get('/solution', (req,res) => {
+  res.json(solution)
+})
 
 app.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`)
