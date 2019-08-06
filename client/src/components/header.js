6f9b002315c91f06b4d3a7339aff41222d408e7d
OHFloree <2001.florian.mueller@gmail.com>
2019-08-06 13:33:43 +0200

    created prototype

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,17 @@
 import React, {Fragment} from 'react';
+import styled from 'styled-components'
 
 function App() {
   return (
-    <Fragment>
-      
-    </Fragment>
+    <HeaderWrapper>
+
+    </HeaderWrapper>
   );
 }
 
 export default App;
+
+const HeaderWrapper = styled.div`
+  grid-row: 1;
+  background-color: #620000;
+`
