e0c2ef3beaab0e56fe8f9b180df1abb4f8dd297a
OHFloree <2001.florian.mueller@gmail.com>
2019-09-11 17:33:46 +0200

    Settings Menu

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,38 @@
 import React, {Component, Fragment} from 'react';
-import SocketContext from './context/socket-context.js'
 import io from 'socket.io-client';
 import styled from 'styled-components'
+import Context from './context/context.js'
+
 import Header from './components/header.js'
 import GameField from './components/gamefield.js'
-import PinPlacement from './components/placement'
-
-const socket = io.connect('localhost:5000')
+import PinPlacement from './components/placement.js'
+import Menu from './components/menu.js'
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
-      pause: false
+      pause: false,
+      handlePause: this.handlePause,
+      socket: io.connect('localhost:5000')
     }
   }
 
-  getPause = (pause) => {
-    this.setState({pause: pause})
+  handlePause = () => {
+    console.log(this.state.pause);
+    this.setState({pause: !this.state.pause})
   }
 
   render() {
     return (
-      <SocketContext.Provider value = {socket}>
+      <Context.Provider value = {this.state}>
         <Wrapper>
-          {this.state.pause ? <Overlay /> : null}
-          <Header getPause={this.getPause} />
+          {this.state.pause ? <Overlay><Menu /></Overlay> : null}
+          <Header />
           <GameField />
           <PinPlacement />
         </Wrapper>
-      </SocketContext.Provider>
+      </Context.Provider>
     );
   }
 }
@@ -52,4 +55,7 @@ const Overlay = styled.div`
   width: 100vw;
   height: 100vh;
   background-color: rgba(0,0,0,0.7);
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `
