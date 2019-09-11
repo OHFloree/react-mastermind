b7beec7961a918aea9c5817d157ee1c2a0803cac
OHFloree <2001.florian.mueller@gmail.com>
2019-09-11 22:14:14 +0200

    npm build

---
+++ client/build/precache-manifest.5cd1f28b76607b5d81c25983feb18b08.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5687349da956b42844dd82d729397d0a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7683168b678fa92b98c1",
+    "url": "/static/css/main.92495174.chunk.css"
+  },
+  {
+    "revision": "05b9c25531310f85a572",
+    "url": "/static/js/2.191cc0fb.chunk.js"
+  },
+  {
+    "revision": "7683168b678fa92b98c1",
+    "url": "/static/js/main.a6de82e3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a462ec8fbeb5bdd65600bca448a0c378",
+    "url": "/static/media/logo.a462ec8f.svg"
+  }
+]);
