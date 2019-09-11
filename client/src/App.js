b45c7c20907be6cabc56a665ce45a7a7a112c59e
OHFloree <2001.florian.mueller@gmail.com>
2019-09-11 21:45:53 +0200

    test

--- client/src/App.js
+++ client/src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
     this.state = {
       pause: false,
       handlePause: this.handlePause,
-      socket: io('http://192.168.0.0.115:80')
+      socket: io('http://192.168.2.107:80')
     }
   }
 
