f636d903090d56bcc86eba7e1a04fd15e6634e07
OHFloree <2001.florian.mueller@gmail.com>
2019-09-11 22:13:22 +0200

    test

--- server.js
+++ server.js
@@ -10,9 +10,9 @@ if(process.env.NODE_ENV === 'production') {
   app.use(express.static('client/build'));
 }
 
-/*app.get("*", (req, res) => {
+app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-});*/
+});
 
 server.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
