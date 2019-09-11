1ae45b0071dbec4e71df36314c07cadfee6fa37e
OHFloree <2001.florian.mueller@gmail.com>
2019-09-11 21:25:06 +0200

    server setup for deployment

--- server.js
+++ server.js
@@ -5,6 +5,9 @@ const server = require('http').Server(app);
 const io = require('socket.io')(server);
 const PORT = process.env.PORT || 5000;
 
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
+}
 
 server.listen(PORT, () => {
   console.log(`listening on PORT: ${PORT}`);
