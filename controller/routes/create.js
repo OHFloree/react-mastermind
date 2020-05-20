6f97665a51d24ede8b699cbfdef6d6f53ba16366
OHFloree <2001.florian.mueller@gmail.com>
2020-05-20 23:41:25 +0200

    solution is stored in session

--- controller/routes/create.js
+++ controller/routes/create.js
@@ -2,11 +2,12 @@ const SolutionService = require('../../services/solutionService.js')
 
 
 module.exports = (app) => {
-    app.get('/create', async (req,res) => {
+    app.get('/create-game', async (req,res) => {
         try {
             const solutionService = new SolutionService
-            let solution =  await solutionService.generateSolution()
-            res.json(solution)
+            const solution = await solutionService.generateSolution()
+            req.session.solution = solution
+            res.sendStatus(201)
         }
         catch(e) {
             res.status(404).json({
