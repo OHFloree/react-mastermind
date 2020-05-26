193251e00af5a53f70ff87d003bdbc613666f810
OHFloree <2001.florian.mueller@gmail.com>
2020-05-26 15:35:05 +0200

    layout prototype

--- client/src/components/GameField.js
+++ client/src/components/GameField.js
@@ -1,22 +1,68 @@
 import React, { useContext, Fragment } from 'react'
 import { FeedbackObjectContext } from '../context/feedbackObjectContext'
+import styled from 'styled-components'
 
 function GameField() {
     const { feedbackObject } = useContext(FeedbackObjectContext)
 
     return (
-        <Fragment>
-            {feedbackObject.map(feedbackRow => (
-                <div>{feedbackRow.guess.map(guessField => {
-                    return <div style={{ backgroundColor: guessField }}>{guessField}</div>
-                })}
-                    <p>{feedbackRow.feedback[0]}</p>
-                    <p>{feedbackRow.feedback[1]}</p>
-                </div>
+        <Field>
+            {feedbackObject.map((feedbackRow, i) => (
+                <Row key={i}>
+                    <Guess>
+                        {feedbackRow.guess.map((guessField, j) => {
+                            return <GuessPin key={j} color={guessField}></GuessPin>
+                        })}
+                    </Guess>
+                    <Feedback>
+                        <p>{feedbackRow.feedback[0]}</p>
+                        <p>{feedbackRow.feedback[1]}</p>
+                    </Feedback>
+                </Row>
             ))}
-        </Fragment>
+        </Field>
     )
 }
 
+const Field = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    overflow-y: auto;
+    overflow-y: overlay;
+`
+
+const Row = styled.div`
+    width: 50%;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+`
+
+const Guess = styled.div`
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly;
+`
+
+const GuessPin = styled.div`
+    height: 40px;
+    width: 40px;
+    background-color: ${props => props.color};
+`
+
+const Feedback = styled.div`
+    width: 40px;
+    height: 40px;
+`
+
 export default GameField
 
+/*   -ms-overflow-style: none;
+
+::-webkit-scrollbar {
+    display: none;
+}*/
